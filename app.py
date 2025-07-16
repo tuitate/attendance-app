a0a1f98c74338b20f7cdb3f8d82d9c4f7f3b977e
tuitate <tuikap5634789@gmail.com>
2025-07-16 18:44:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -700,7 +700,6 @@ def show_shift_table_page():
         return styles
 
     styled_df = df.style.apply(highlight_user, name_to_highlight=current_user_display_name, subset=['従業員名'])
-    st.dataframe(styled_df, use_container_width=True, hide_index=True)
 
 def show_direct_message_page():  
     selected_user_id = st.session_state.get('dm_selected_user_id')
